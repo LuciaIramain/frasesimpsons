c91b182be071571f74abae3a08716761193530aa
Lucia Iramain <luciairamain5@gmail.com>
2021-05-06 15:22:54 -0300

    Se agrego el componente spinner y su logica para mostrar el spinner o la frase y tambien logica de frase.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,45 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Frase from './components/Frase';
+import React, {useState, useEffect} from 'react';
+import Spinner from './components/Spinner';
 
 function App() {
+  // Crear los state adecuados
+  const [personaje, setPersonaje] = useState({});
+  const [cargando, setCargando] = useState(false);
+
+  // ciclo de vida
+  useEffect(()=> {
+    // Aqui va la logica
+    consultaAPI();
+  },[]);
+
+  const consultaAPI = async() => {
+    // Aqui quiero mostrar el spinner
+    setCargando(true);
+    const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+    const result = await respuesta.json();
+    console.log(result[0]);
+    setTimeout(() => {
+       // guardar el resultado de mi consulta en el state
+      setPersonaje(result[0]);
+      // Quiero mostrar el componente frase
+      setCargando(false);
+    }, 2000);
+  };
+  // operador ternario (funciona como un if) tengo que poner si o si las dos condiciones
+  // (condicion logica) ? (que pasa si la condición se cumple) : (que pasa si la condición no se cumple)
+  const mostrarComponente = (cargando) ? (<Spinner/>) : (<Frase propiedadPersonaje = {personaje}/>);
+
   return (
     <section className='container text-center my-5'>
       <article className='d-flex flex-column align-items-center'>
         <img src={process.env.PUBLIC_URL + 'logo.png'} alt="Logo de los simpsons" className='w-75 mb-5'/> 
         {/* process.env(ambiente en donde esta).PUBLIC_URL es para utilizar imagenes que están en una carpeta pública */}
-        <Button variant="warning" className='w-75 text-secondary mb-5'>Obtener Frase</Button>
+        <Button variant="warning" className='w-75 text-secondary mb-5' onClick={() => consultaAPI()}>Obtener Frase</Button>
       </article>
-      <Frase />
+      {mostrarComponente}
     </section>
   );
 }
--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
+  
+import React from "react";
+import Card from "react-bootstrap/Card";
 
-const Frase = () => {
-    return (
-        <Card>
-            <Card.Body>
-                <div className="row">
-                    <div className="col-sm-12 col-md-4">
-                        <img src={''} alt=""/>
-                    </div>
-                    <div className="col-sm-12 col-md-8">
-                        <Card.Title>Nombre del personaje</Card.Title>
-                        <Card.Text>
-                            Frase aleatoria
-                        </Card.Text>
-                    </div>
-                </div>
-            </Card.Body>
-        </Card>
-    );
+const Frase = (props) => {
+  return (
+    <Card>
+      <Card.Body>
+        <div className="row">
+          <div className="col-sm-12 col-md-4">
+            <img src={props.propiedadPersonaje.image} alt={props.propiedadPersonaje.character} />
+          </div>
+          <div className="col-sm-12 col-md-8">
+            <Card.Title>{props.propiedadPersonaje.character}</Card.Title>
+            <Card.Text>{props.propiedadPersonaje.quote}</Card.Text>
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Frase;
